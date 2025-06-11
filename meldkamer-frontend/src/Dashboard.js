73d76e67a36152555dac6bc4c34bddb012fc12e3
Gittiyap <Mooijweer@proton.me>
2025-06-11 09:47:49 +0200

    Update Dashboard.js

--- meldkamer-frontend/src/Dashboard.js
+++ meldkamer-frontend/src/Dashboard.js
@@ -88,7 +88,8 @@ const Dashboard = () => {
 
   // Ophalen alle meldingen 1x
   useEffect(() => {
-    console.log("https://meldkamer-back-end.onrender.com:", API_URL); axios.get(API_URL).then((response) => {
+    console.log("API_URL:", API_URL);
+    axios.get(API_URL).then((response) => {
       setAlleMeldingen(response.data);
       setZichtbareMeldingen([]);
       setNoodgevallen([]);
